147fd4fe7ada105ea21ce7eae1ba16b15c7293bb
talm <talmontequie@gmail.com>
2019-06-13 11:16:38 +0300

    added files from zip

---
+++ src/edu/cg/models/Car/F1Car.java
@@ -0,0 +1,20 @@
+package edu.cg.models.Car;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import com.jogamp.opengl.*;
+
+import edu.cg.algebra.Point;
+import edu.cg.models.IRenderable;
+
+/**
+ * A F1 Racing Car.
+ *
+ */
+public class F1Car implements IRenderable {
+	// TODO: Put your implementation from previous exercise.
+	// 		 * We deleted all components source files (Back.java, Center.java...), so put your components implementation as well.
+	//       * You also need to setup material properties for different
+	//       * components of the car.
+}
---
+++ src/edu/cg/models/SkewedBox.java
@@ -0,0 +1,15 @@
+package edu.cg.models;
+
+import java.io.File;
+
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.util.texture.Texture;
+import com.jogamp.opengl.util.texture.TextureIO;
+
+import edu.cg.algebra.Vec;
+
+public class SkewedBox implements IRenderable {
+	// TODO: Add you implementation from previous exercise.
+	//       * Note you may want to enable textures here to render
+	//         the wooden boxes.
+}
